3027c77ad57921d09594a0b7063a689252d538ab
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-03-04 19:46:39 +0530

    feat: Navbar added to dynamic

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/Media/logo.png'
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false)
+
+  useEffect (()=>{
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    })
+  },[])
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky? 'nav-dark' : ''}`}>
         <img src={logo} alt="logo.png" className='logo' />
         <ul>
             <li>Home</li>
